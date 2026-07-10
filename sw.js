0609a9f813a1338033c5d7b6c0f6366d14b7c3aa
sxjm <2361609925>
2026-07-10 20:52:16 +0800

    Site updated: 2026-07-10 20:52:15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1783687853006";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1783687915418";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
